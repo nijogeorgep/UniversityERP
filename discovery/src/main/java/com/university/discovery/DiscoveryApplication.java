4ce5152822d9c8b8448eb060e59486122b3895ef
nijogeorgep <nijomon@gmail.com>
2018-11-23 22:00:19 +0530

    Maven Architecture Changed

--- discovery/src/main/java/com/university/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/university/discovery/DiscoveryApplication.java
@@ -3,11 +3,9 @@ package com.university.discovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableAdminServer
 public class DiscoveryApplication {
 
   public static void main(String[] args) {
